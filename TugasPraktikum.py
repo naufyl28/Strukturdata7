189dc40f53e3e09e8bc54c342cfb1b64db5515ab
Muhammad Naufal Muhyiddin <108875369+naufyl28@users.noreply.github.com>
2022-09-29 13:56:29 +0800

    Bikin kelas linklist dan insert nya

---
+++ TugasPraktikum.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def insert(self, data):
+        node = Node(data)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+
+    def printlist(self):
+        node = self.head
+        while node:
+            print(node.data)
+            node = node.nex
